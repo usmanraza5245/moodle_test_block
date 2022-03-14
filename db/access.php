abd4ecc85a2448561afc50dc100541cb63addab3
raza-usman <usman.raza@codistan.org>
2022-03-14 15:05:46 +0500

    test block of moode

---
+++ db/access.php
@@ -0,0 +1,26 @@
+<?php
+$capabilities = array(
+
+    'block/test_block:myaddinstance' => array(
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => array(
+            'user' => CAP_ALLOW
+        ),
+
+        'clonepermissionsfrom' => 'moodle/my:manageblocks'
+    ),
+
+    'block/test_block:addinstance' => array(
+        'riskbitmask' => RISK_SPAM | RISK_XSS,
+
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+
+        'clonepermissionsfrom' => 'moodle/site:manageblocks'
+    ),
+);
---
+++ lang/en/block_test_block.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Strings for component 'block_test_block', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   block_test_block
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+$string['test_block:addinstance'] = 'Add a new test block';
+$string['test_block:myaddinstance'] = 'Add a new test block to Dashboard';
+$string['test_block'] = 'test_block';
+$string['pluginname'] = 'test_block';
+$string['search:content'] = 'test block content';
+$string['privacy:metadata:block'] = 'The test block stores all of its data within the block subsystem.';
